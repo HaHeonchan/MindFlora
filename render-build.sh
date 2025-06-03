0d35618b0e6a0b174a17b6e80eb3265b5b6e3f77
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2025-06-03 22:47:35 +0900

    fix: sqlite3.o를 C로 컴파일 하도록 변경

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "🔧 mymodule.so 생성 중..."
+echo "🔧 [1/2] sqlite3.o C로 컴파일 중..."
+gcc -c -fPIC -DSQLITE_OMIT_JSON -o ./data/sqlite/sqlite3.o ./data/sqlite/sqlite3.c -I./data/sqlite
 
-g++ -shared -fPIC -DSQLITE_OMIT_JSON -o mymodule.so yoon.cpp \
-  ./data/sqlite/sqlite3.c \
+echo "🔧 [2/2] mymodule.so C++로 링크 중..."
+g++ -shared -fPIC -o mymodule.so yoon.cpp \
+  ./data/sqlite/sqlite3.o \
   -I./data/sqlite \
   -I/usr/include/x86_64-linux-gnu/curl \
   -I./data/json \
