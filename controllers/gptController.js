8e4d958faf955974b13963023a58acc4ec9435fe
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2025-06-15 17:07:42 +0900

    fix : db에 채팅 기록이 남지 않던 문제 수정

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -16,6 +16,7 @@ const Summary = require("../db/summary");
 const diaryReplyDB = require("../db/diaryReply");
 const LongTermMemory = require("../db/longTermMemory");
 const { json } = require("stream/consumers");
+const chat = require("../db/chat");
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
@@ -218,6 +219,8 @@ ${historyText}
 
   const analyze_text_result_ai = JSON.parse(lib.analyze_text(content, api_key));
 
+
+
   //전체 기억 저장장
   const memoryDocs = [
     { uid: userId, role: "user", content: message },
@@ -226,6 +229,7 @@ ${historyText}
   await Memory.insertMany(memoryDocs);
   chatMemory[userId].push(...memoryDocs.map(({ role, content }) => ({ role, content })));
 
+  await Chat.create({ uid: userId, reqText: message, resText: content, sender: "user" });
   //장기기억 갱신
   memorize(userId, analyze_text_result_user, analyze_text_result_ai, message, content)
 
